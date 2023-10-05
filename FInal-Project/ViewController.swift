1a43b812bf1dfc0fdebaa4502d5e96c4b8e8b54e
Kevin Infante <kevininfante90@gmail.com>
2023-10-05 11:40:22 -0400

    Initial Commit

---
+++ FInal-Project/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FInal-Project
+//
+//  Created by Kevin Infante on 10/5/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ FInal-ProjectUITests/FInal_ProjectUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  FInal_ProjectUITestsLaunchTests.swift
+//  FInal-ProjectUITests
+//
+//  Created by Kevin Infante on 10/5/23.
+//
+
+import XCTest
+
+final class FInal_ProjectUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}

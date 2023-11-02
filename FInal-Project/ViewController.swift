113140f2e628c73836450ab6bd1369332eab1baf
Kevin Infante <kevininfante90@gmail.com>
2023-11-02 17:16:32 -0400

    Implemented a basic map view, but the "Lookaround/streetview" still needs work

--- FInal-Project/ViewController.swift
+++ FInal-Project/ViewController.swift
@@ -6,12 +6,80 @@
 //
 
 import UIKit
+import MapKit
+import CoreLocation  // Import CoreLocation to access the CLLocationManagerDelegate protocol
 
-class ViewController: UIViewController {
 
+
+class ViewController: UIViewController, CLLocationManagerDelegate {
+    
+    @IBOutlet weak var lookAround: UIButton!
+    @IBOutlet weak var mapView: MKMapView!
+    
+    let locationManager = CLLocationManager()
+    
     override func viewDidLoad() {
+        // the code below fetches info from an API
+        // Create a search URL for fetching cities
+        let url = URL(string: "https://wft-geo-db.p.rapidapi.com/v1/geo/cities/123214?rapidapi-key="+api_key)!
+        let request = URLRequest(url: url)
+        
+        let task = URLSession.shared.dataTask(with: request) { [weak self] data, response, error in
+
+            // Handle any errors
+            if let error = error {
+                print("❌ Network error: \(error.localizedDescription)")
+            }
+
+            // Make sure we have data
+            guard let data = data else {
+                print("❌ Data is nil")
+                return
+            }
+
+            do {
+                let jsonDictionary = try JSONSerialization.jsonObject(with: data) as? [String: Any]
+                print(jsonDictionary)
+            } catch {
+                print("❌ Error parsing JSON: \(error.localizedDescription)")
+            }
+        }
+        // Initiate the network request
+        task.resume()
+        
+        
+        
+        
+        
+        
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        locationManager.delegate = self
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.startUpdatingLocation()
+        
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        if let location = locations.first {
+            let coordinate = location.coordinate
+            let span = MKCoordinateSpan(latitudeDelta: 0.005, longitudeDelta: 0.005)
+            let region = MKCoordinateRegion(center: coordinate, span: span)
+            mapView.setRegion(region, animated: true)
+        }
+    }
+
+    func enableLookAround() {
+        let camera = MKMapCamera()
+        camera.centerCoordinate = mapView.region.center
+        camera.heading = 0  // Adjust this value as needed to control the initial orientation
+        camera.pitch = 80   // Adjust this value as needed to control the initial pitch
+
+        mapView.camera = camera
+    }
+
+    @IBAction func startLookAround(_ sender: UIButton) {
+        enableLookAround()
     }
 
 
---
+++ FInal-Project/api.swift
@@ -0,0 +1,12 @@
+//
+//  api.swift
+//  FInal-Project
+//
+//  Created by Kevin Infante on 10/24/23.
+//
+
+import Foundation
+
+let api_key = "0de85ba02dmsh43b73f514991d0bp13f7e1jsn43433488a041"
+
+//make a struct that will access the different objects of the api response

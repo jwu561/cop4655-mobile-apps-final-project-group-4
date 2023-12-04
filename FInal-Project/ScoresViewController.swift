d14e54efaea3bd2d39f16ee4931ef8cdcfc3b6e5
Jonathan Wu <jonwu561@gmail.com>
2023-12-03 21:28:35 -0500

    added log out button to scoreboard

--- FInal-Project/ScoresViewController.swift
+++ FInal-Project/ScoresViewController.swift
@@ -43,4 +43,24 @@ class ScoresViewController: UIViewController {
     }
     */
 
+    @IBAction func onLogOutTapped(_ sender: Any) {
+        showConfirmLogoutAlert()
+    }
+
+    private func showConfirmLogoutAlert() {
+        let alertController = UIAlertController(title: "Log out of your account?", message: nil, preferredStyle: .alert)
+        let logOutAction = UIAlertAction(title: "Log out", style: .destructive) { _ in
+            NotificationCenter.default.post(name: Notification.Name("logout"), object: nil)
+        }
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel)
+        alertController.addAction(logOutAction)
+        alertController.addAction(cancelAction)
+        present(alertController, animated: true)
+    }
+    
+    
+    
+    
+    
+    
 }
